3719f33a0ae8c99d4129465087d98083c75a7795
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-02-15 15:38:23 -0500

    Add project files.

---
+++ CellPhone/CellPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CellPhone {
+    class CellPhone {
+        public string PhoneNumber { get; set; }
+        public string Serviceprovider { get; set; }
+        public string OperatingSystem { get; set; }
+        public bool Active { get; set; }
+        public int Capacity { get; set; }
+        public string Model { get; set; }
+
+    }
+}
---
+++ CellPhone/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CellPhone {
+    class Program {
+        static void Main(string[] args) {
+
+            var cp1 = new CellPhone {
+                PhoneNumber = "513-555-1212",
+                Serviceprovider = "Verizon",
+                OperatingSystem = "iOS",
+                Capacity = 128,
+                Model = "iPhone X"
+            };
+            var cp2 = new CellPhone {
+                PhoneNumber = "614-555--1212",
+                Serviceprovider = "T-Mobile",
+                OperatingSystem = "Android",
+                Capacity = 64,
+                Model = "Samsung s6"
+            };
+
+
+            var cellphones = new CellPhone[] { cp1, cp2 };
+
+            foreach (var CP in cellphones) {
+                Console.WriteLine($"Phone Number is {CP.PhoneNumber}");
+            }
+        }
+    }
+}
